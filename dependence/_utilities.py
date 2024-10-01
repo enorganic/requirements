dc9109c1aeb81e613d17344e12b6066531e9fcda
David Belais <davebelais@users.noreply.github.com>
2024-10-01 11:04:06 -0700

    Fix version detection for editable installs (#15)

--- dependence/_utilities.py
+++ dependence/_utilities.py
@@ -1,25 +1,23 @@
 import sys
-from functools import lru_cache
+from functools import lru_cache, wraps
 from itertools import chain
-from subprocess import DEVNULL
-from subprocess import check_output as _check_output
+from pathlib import Path
+from subprocess import DEVNULL, PIPE, list2cmdline, run
 from traceback import format_exception
-from typing import Any, Dict, Hashable, Iterable, List, Set, Tuple
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Hashable,
+    Iterable,
+    List,
+    Set,
+    Tuple,
+    Union,
+)
+from warnings import warn
 
 import tomli
-
-
-def check_output(command: Tuple[str, ...]) -> str:
-    """
-    This function wraps `subprocess.check_output`, but redirects stderr
-    to a temporary file, then deletes that file (a platform-independent
-    means of redirecting output to DEVNULL).
-
-    Parameters:
-
-    - command (Tuple[str, ...]): The command to run
-    """
-    return _check_output(command, stderr=DEVNULL, text=True)
 
 
 def iter_distinct(items: Iterable[Hashable]) -> Iterable:
@@ -97,3 +95,81 @@ def iter_parse_delimited_values(
         return _iter_parse_delimited_value(value, delimiter=delimiter)
 
     return chain(*map(iter_parse_delimited_value_, values))
+
+
+def check_output(
+    args: Tuple[str, ...],
+    cwd: Union[str, Path] = "",
+    echo: bool = False,
+) -> str:
+    """
+    This function mimics `subprocess.check_output`, but redirects stderr
+    to DEVNULL, and ignores unicode decoding errors.
+
+    Parameters:
+
+    - command (Tuple[str, ...]): The command to run
+    """
+    if echo:
+        if cwd:
+            print("$", "cd", cwd, "&&", list2cmdline(args))
+        else:
+            print("$", list2cmdline(args))
+    output: str = run(
+        args,
+        stdout=PIPE,
+        stderr=DEVNULL,
+        check=True,
+        cwd=cwd or None,
+    ).stdout.decode("utf-8", errors="ignore")
+    if echo:
+        print(output)
+    return output
+
+
+def get_qualified_name(function: Callable[..., Any]) -> str:
+    name: str = getattr(function, "__name__", "")
+    if name:
+        module: str = getattr(function, "__module__", "")
+        if module not in (
+            "builtins",
+            "__builtin__",
+            "__main__",
+            "__init__",
+            "",
+        ):
+            name = f"{module}.{name}"
+    return name
+
+
+def deprecated(message: str = "") -> Callable[..., Callable[..., Any]]:
+    """
+    This decorator marks a function as deprecated, and issues a
+    deprecation warning when the function is called.
+    """
+
+    def decorating_function(
+        function: Callable[..., Any]
+    ) -> Callable[..., Any]:
+
+        @wraps(function)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            name: str = get_qualified_name(function)
+            warn(
+                (
+                    (
+                        f"{name} is deprecated: {message}"
+                        if message
+                        else f"{name} is deprecated"
+                    )
+                    if name
+                    else message
+                ),
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            return function(*args, **kwargs)
+
+        return wrapper
+
+    return decorating_function
